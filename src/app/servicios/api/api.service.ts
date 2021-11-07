9f8dc629178736de88116d090ea5db6b15c21314
albi.penayo <albi.penayo@gmail.com>
2021-11-06 23:13:27 -0300

    login ok

--- src/app/servicios/api/api.service.ts
+++ src/app/servicios/api/api.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/entidades/usuario';
+import { UserServiceService } from './user-service.service';
+import { map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +13,16 @@ export class ApiService {
   usuario!: Usuario;
   api: string;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private map: UserServiceService){
     this.api="http://localhost:666";
+   // this.api="https://apipenayo.herokuapp.com";
   }
 
   login(usuario:Usuario): Observable<Usuario[]>{
     return <Observable<Usuario[]>>(this.http.post(this.api+'/usuario/login',JSON.stringify(usuario)));
   }
-  
+
+  sesion(user: Usuario): Observable<Usuario[]>{
+    return <Observable<Usuario[]>>(this.http.post(this.api+'/usuario/login',JSON.stringify(user)));
+  }
 }
--- src/app/servicios/api/user-service.service.ts
+++ src/app/servicios/api/user-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from 'src/app/entidades/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-
+  usuario!: Usuario;
   constructor() { }
+
+  setUsuario(usuario: Usuario){
+    this.usuario = usuario;
+  } 
+  getUsuario(){
+    return this.usuario;
+  }
 }
