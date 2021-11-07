af3dfa179c61daacde685acc8914aafc8fb8a7ff
albi.penayo <albi.penayo@gmail.com>
2021-11-07 09:15:48 -0300

    prod

--- src/app/servicios/api/producto-service.service.ts
+++ src/app/servicios/api/producto-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { Productos } from 'src/app/entidades/productos';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoServiceService {
-
+  producto!: Productos;
   constructor() { }
+  setProducto(producto: Productos){
+    this.producto = producto;
+  } 
+  getProducto(){
+    return this.producto;
+  }
 }
